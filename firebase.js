20db2018fc396c16f4aa0e14a55c1c29f49752b8
Tyler <tylerlarson919@gmail.com>
2024-08-16 02:02:56 -0400

    finished v1

--- firebase.js
+++ firebase.js
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage"; // Import Firebase Storage
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "d13cf4aa75e61e62c52c8fca858d35dbe7348c4d",
+  authDomain: "focusflow-693a5.firebaseapp.com",
+  projectId: "focusflow-693a5",
+  storageBucket: "focusflow-693a5.appspot.com",
+  messagingSenderId: "824288440733",
+  appId: "1:824288440733:web:94428e172841bfe481bf74",
 };
 
 // Initialize Firebasegs://tradingty-6ed07.appspot.com
